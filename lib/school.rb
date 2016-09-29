a4ce821a83469e62dd8e6b5a9e6f7634cc9bf84a
Flatiron School <flatironschool@Drew-2.local>
2016-09-29 14:04:07 -0400

    Complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade, student_list| student_list.sort!}
+  end
+end
+
+mine = School.new("Drew U")
+binding.pry
+
+puts "hola"
